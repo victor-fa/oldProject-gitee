ea582a6003b3ddb855493c7b02c7b2df53bc3744
BigYuki <bigyuki@yeah.net>
2017-05-13 00:07:43 +0800

    Add animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,7 +14,8 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/main/components/about/about.component.ts
+++ src/app/modules/main/components/about/about.component.ts
@@ -1,9 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import {trigger, state, style, animate, transition} from '@angular/animations';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  animations: [
+    trigger('flyInOut', [
+      state('in', style({
+          opacity: 1,
+          transform: 'translateX(0)'
+        })),
+      state('out',   style({
+          opacity: 0,
+          transform: 'translateX(50%)'
+        })),
+      transition('in => out', animate('100ms ease-in', style(
+          {
+              transform: 'translateX(-50%)'
+          }
+      ))),
+      transition('out => in', animate('100ms ease-out'))
+    ])
+  ]
 })
 export class AboutComponent implements OnInit {
 
