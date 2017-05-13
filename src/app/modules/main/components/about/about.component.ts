2ab92d53212fd13638e3226739c16754dc4a0e31
BigYuki <bigyuki@yeah.net>
2017-05-13 10:47:56 +0800

    Fix bugs

--- src/app/modules/main/components/about/about.component.ts
+++ src/app/modules/main/components/about/about.component.ts
@@ -1,41 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-import {trigger, state, style, animate, transition} from '@angular/animations';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+import { TimerObservable } from "rxjs/observable/TimerObservable";
+import { Subscription } from "rxjs/Subscription";
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss'],
-  animations: [
-    trigger('flyInOut', [
-      state('in', style({
-          opacity: 1,
-          transform: 'translateX(0)'
-        })),
-      state('out',   style({
-          opacity: 0,
-          transform: 'translateX(50%)'
-        })),
-      transition('in => out', animate('100ms ease-in', style(
-          {
-              transform: 'translateX(-50%)'
-          }
-      ))),
-      transition('out => in', animate('100ms ease-out'))
-    ])
-  ]
+    selector: 'app-about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.scss'],
+    animations: [
+        trigger('flyInOut', [
+            state('in', style({
+                opacity: 1,
+                transform: 'translateX(0)'
+            })),
+            state('out', style({
+                opacity: 0,
+                transform: 'translateX(50%)'
+            })),
+            transition('in => out', animate('200ms ease-in', style(
+                {
+                    transform: 'translateX(-50%)'
+                }
+            ))),
+            transition('out => in', animate('200ms ease-out'))
+        ])
+    ]
 })
 export class AboutComponent implements OnInit {
 
-  hideAppointment:boolean = true;
-  nowPageNumber:number = 1;
+    hideAppointment: boolean = true;
+    nowPageNumber: number = 1;
 
-  constructor() { }
+    private subscription3s: Subscription;
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        let timer3s = TimerObservable.create(3000,3000);
+        this.subscription3s = timer3s.subscribe(this.refresh3s.bind(this));
+    }
 
-  changePage(i:number){
-      this.nowPageNumber = i;
-  }
+    changePage(i: number) {
+        this.nowPageNumber = i;
+    }
+
+    refresh3s(t: number){
+        this.nowPageNumber = this.nowPageNumber % 3 + 1;
+    }
 
 }
