092cddb10e633827e5667fb3c1eac8189927507b
Yuancheng Zhang <old.far.hot@gmail.com>
2017-06-06 21:40:42 -0500

    change plan-m CSS to SCSS

--- src/app/modules/mobile/components/plan-m/plan-m.component.ts
+++ src/app/modules/mobile/components/plan-m/plan-m.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-plan-m',
   templateUrl: './plan-m.component.html',
-  styleUrls: ['./plan-m.component.css']
+  styleUrls: ['./plan-m.component.scss']
 })
 export class PlanMComponent implements OnInit {
 
