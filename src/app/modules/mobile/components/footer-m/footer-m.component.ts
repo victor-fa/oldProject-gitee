5eeb5a0d67960f00bd41898fe058b0cb70b01ed6
Yuancheng Zhang <old.far.hot@gmail.com>
2017-06-06 22:12:34 -0500

    update footer-m

--- src/app/modules/mobile/components/footer-m/footer-m.component.ts
+++ src/app/modules/mobile/components/footer-m/footer-m.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer-m.component.scss']
 })
 export class FooterMComponent implements OnInit {
-
+  year: number = new Date().getFullYear();
   constructor() { }
 
   ngOnInit() {
