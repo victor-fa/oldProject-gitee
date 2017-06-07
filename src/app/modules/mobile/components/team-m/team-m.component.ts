cc7e28634a408ce194311ee89bd88e029ac013f8
Yuancheng Zhang <old.far.hot@gmail.com>
2017-06-07 10:49:04 -0500

    change team-m CSS to SCSS

--- src/app/modules/mobile/components/team-m/team-m.component.ts
+++ src/app/modules/mobile/components/team-m/team-m.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-team-m',
   templateUrl: './team-m.component.html',
-  styleUrls: ['./team-m.component.css']
+  styleUrls: ['./team-m.component.scss']
 })
 export class TeamMComponent implements OnInit {
 
