0a0af80442d17ede371a4b5f3f5aff7f1feda13c
Yuancheng Zhang <old.far.hot@gmail.com>
2017-06-06 22:20:44 -0500

    change contact-m CSS to SCSS

--- src/app/modules/mobile/components/contact-m/contact-m.component.ts
+++ src/app/modules/mobile/components/contact-m/contact-m.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact-m',
   templateUrl: './contact-m.component.html',
-  styleUrls: ['./contact-m.component.css']
+  styleUrls: ['./contact-m.component.scss']
 })
 export class ContactMComponent implements OnInit {
 
