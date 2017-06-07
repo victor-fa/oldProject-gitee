b19b520511ed9ac13778ddf8de9869423c2a55be
Yuancheng Zhang <old.far.hot@gmail.com>
2017-06-06 21:43:52 -0500

    change footer-m CSS to SCSS

--- src/app/modules/mobile/components/footer-m/footer-m.component.ts
+++ src/app/modules/mobile/components/footer-m/footer-m.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer-m',
   templateUrl: './footer-m.component.html',
-  styleUrls: ['./footer-m.component.css']
+  styleUrls: ['./footer-m.component.scss']
 })
 export class FooterMComponent implements OnInit {
 
