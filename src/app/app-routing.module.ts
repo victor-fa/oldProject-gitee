c6bf585830e2a40a7507dc2dd62357733aab0046
Yuancheng Zhang <old.far.hot@gmail.com>
2017-06-06 20:55:28 -0500

    add routing to mobile version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule }             from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-    { path: '', loadChildren: './modules/main/main.module#MainModule' },
+    { path: 'd', loadChildren: './modules/main/main.module#MainModule' },
+    { path: '', loadChildren: './modules/mobile/mobile.module#MobileModule'},
     { path: 'not-found', loadChildren: './modules/not-found/not-found.module#NotFoundModule' },
     { path: '**', redirectTo: 'not-found' }
 ];
