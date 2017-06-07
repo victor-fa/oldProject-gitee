c68fdea6aa8cd17da156b73feb8c1d5488a76212
Yuancheng Zhang <old.far.hot@gmail.com>
2017-06-07 15:32:54 -0500

    change default layout to Desktop (Mobile layout is /m)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule }             from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-    { path: 'd', loadChildren: './modules/main/main.module#MainModule' },
-    { path: '', loadChildren: './modules/mobile/mobile.module#MobileModule'},
+    { path: '', loadChildren: './modules/main/main.module#MainModule' },
+    { path: 'm', loadChildren: './modules/mobile/mobile.module#MobileModule'},
     { path: 'not-found', loadChildren: './modules/not-found/not-found.module#NotFoundModule' },
     { path: '**', redirectTo: 'not-found' }
 ];
