d27e0ce32230a6e5d782dc67cffbcf79ccc97010
Yuancheng Zhang <old.far.hot@gmail.com>
2017-06-06 21:40:22 -0500

    update about-m

--- src/app/modules/mobile/components/about-m/about-m.component.ts
+++ src/app/modules/mobile/components/about-m/about-m.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about-m',
   templateUrl: './about-m.component.html',
-  styleUrls: ['./about-m.component.css']
+  styleUrls: ['./about-m.component.scss']
 })
 export class AboutMComponent implements OnInit {
 
