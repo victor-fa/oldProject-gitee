55a787bfc31755f2effde65396fc98eebd72a55d
Yuancheng Zhang <old.far.hot@gmail.com>
2017-06-13 15:16:47 -0500

    fix the fatal bug!

--- src/app/modules/mobile/components/header-m/header-m.component.ts
+++ src/app/modules/mobile/components/header-m/header-m.component.ts
@@ -6,11 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header-m.component.scss']
 })
 export class HeaderMComponent implements OnInit {
+  activeAbout: boolean = true;
+  activePlan: boolean = false;
+  activePartner: boolean = false;
+  activeTeam: boolean = false;
+  activeContact: boolean = false;
 
-  constructor() { 
+  constructor() {
   }
 
   ngOnInit() {
   }
 
+  scrollTo(hash: string) {
+    location.hash = "#" + hash;
+    this.activeChecker(hash);
+  }
+
+  activeChecker(id: string) {
+    this.activeAbout = (id == 'about-m' ? true : false);
+    this.activeAbout = (id == 'plan-m' ? true : false);
+    this.activePartner = (id == 'partner-m' ? true : false);
+    this.activeTeam = (id == 'team-m' ? true : false);
+    this.activeContact = (id == 'contact-m' ? true : false);
+  }
 }
