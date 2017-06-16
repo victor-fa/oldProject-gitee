528ad2fc1ba1cd47508e697cab8d81ad5fe6b0dc
Yuancheng Zhang <old.far.hot@gmail.com>
2017-06-16 13:17:00 -0500

    try again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MobiGuard } from './mobi.guard';
 
+import * as $ from 'jquery';
+
 @NgModule({
   declarations: [
     AppComponent
