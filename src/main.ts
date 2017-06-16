defd3ab474f988e9e349f1ffe278de6aaceacb93
Yuancheng Zhang <old.far.hot@gmail.com>
2017-06-16 15:28:30 -0500

    update 100

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-/// <reference path="typings/globals/jquery/index.d.ts" />
+/// <reference path="./jquery.d.ts" />
 import * as $ from 'jquery';
 
 if (environment.production) {
