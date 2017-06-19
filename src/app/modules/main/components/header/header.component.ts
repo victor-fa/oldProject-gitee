033778d1272cea7e58f650c51331e49bee6eae45
Yuancheng Zhang <old.far.hot@gmail.com>
2017-06-19 17:04:58 -0500

    修复了navbar点击后的没有active的bug

--- src/app/modules/main/components/header/header.component.ts
+++ src/app/modules/main/components/header/header.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+/// <reference path="jquery.d.ts" />
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements AfterViewInit {
+    constructor() {}
 
-
-    constructor() {
-
+    ngAfterViewInit() {
+        $(document).ready(function () {
+            $('#main-container nav a').click(function (e) {
+                $('#main-container nav a').removeClass('active');
+                var $parent = $(this).addClass('active');
+            });
+        });
     }
-    ngOnInit() {}
-
-
 }
