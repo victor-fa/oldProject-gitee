ac0c27c63f65cc4f425fca1b6cc6a1ec46428dcc
Yuancheng Zhang <old.far.hot@gmail.com>
2017-07-08 13:13:07 -0500

    改成静态的

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require("express")
-var app = express()
-
-var port = process.env.PORT || 10001
-app.set('port', port)
-
-app.listen(app.get('port'), function() {
-    console.log('Server started at ' + port)
-})
