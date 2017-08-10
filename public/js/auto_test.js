fdc0167979c7292937e22676c4009724e47e00bc
Feliciano Long <noahzark@vip.qq.com>
2017-08-10 01:31:50 -0500

    add test counter

--- public/js/auto_test.js
+++ public/js/auto_test.js
@@ -5,6 +5,7 @@ var ask_counter = 0
 function auto_ask() {
     if (ask_counter < test_cases.length) {
         setTimeout(function() {
+            $('#progress').text((ask_counter+1) + ' / ' + test_cases.length)
             ask_question(test_cases[ask_counter++], auto_ask)
         }, 1000)
     }
