6b7494903a2109880d418b619dc3dcc71f9e77e2
Yuancheng Zhang <old.far.hot@gmail.com>
2017-08-15 21:42:39 -0500

    用ajax显示了user list

--- routes/admins.js
+++ routes/admins.js
@@ -2,29 +2,53 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 
-// List All Users
+// Manage
 router.get('/manage', User.ensureAuthenticated, function (req, res) {
 	User.listAllUsers(function (err, users) {
 		if (err) {
 			throw err;
 		}
-		res.render('admin/manage', { 
+		res.render('admin/manage', {
 			users,
 			css: ['/css/qw/manage.css'],
-			js: ['/js/qw/manage.js']
+			js: [
+				'/js/qw/manage.js'
+			]
 		});
 	});
 });
 
-// Active or inactive user
-router.post('/manage', User.ensureAuthenticated, function (req, res) {
+// API: list all users
+router.post('/manage/api/load', User.ensureAuthenticated, function (req, res) {
+	User.listAllUsers(function (err, users) {
+		if (err) {
+			throw err;
+		}
+		res.json(users);
+	});
+});
+
+// API: active or inactive user
+router.post('/manage/api/active', User.ensureAuthenticated, function (req, res) {
 	var id = req.body.id;
 	var active = req.body.active;
 	console.log(`_id = ${id} \n act = ${active}`);
+	User.updateUserActive(id, active, function (err) {
+		if (err) {
+			throw err;
+			// err alert
+			/* ... */
 
-	res.json({
-		retcode: 0
-	})
+			// send err res
+			res.json({
+				retcode: 1,
+				msg: err
+			})
+		}
+		res.json({
+			retcode: 0
+		})
+	});
 });
 
 module.exports = router;
