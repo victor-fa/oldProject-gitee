8a2299b89fdcd9dbff77230ed2cf07c4f161912d
Yuancheng Zhang <old.far.hot@gmail.com>
2017-08-24 18:51:29 -0500

    fixed a bug

--- public/js/qw/manage-detail.js
+++ public/js/qw/manage-detail.js
@@ -1,7 +1,7 @@
 $().ready(function () {
     $('#qw-admin-tool').addClass('in');
 
-    $('.user-app-list-item-remove a').each(function () {
+    $('.user-app-list-item-remove').each(function () {
         var href = $(this).attr('href')+'&id='+$('#user-id').html()
         $(this).attr('href', href);
         console.log(href);
