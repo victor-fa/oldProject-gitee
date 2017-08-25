84166932cbe0324a7d3624210417c7798ba1818e
Yuancheng Zhang <old.far.hot@gmail.com>
2017-08-25 14:30:16 -0500

    修复了register页面出错后无法保存已输入信息的bug

--- routes/users.js
+++ routes/users.js
@@ -29,20 +29,22 @@ router.get('/register', function (req, res) {
 
 // Register
 router.post('/register', function (req, res) {
-	var name = req.body.name;
-	var email = req.body.email;
-	var username = req.body.username;
-	var password = req.body.password;
-	var password2 = req.body.password2;
-	var phone = req.body.phone;
-	var company = req.body.company;
-	var group = 0;
+	var user_info = {};
+	user_info.name = req.body.name;
+	user_info.email = req.body.email;
+	user_info.username = req.body.username;
+	user_info.phone = req.body.phone;
+	user_info.company = req.body.company;
+	password = req.body.password;
+	password2 = req.body.password2;
+	group = 0;
 
 	// Validation
 	req.checkBody('name', '真实姓名不能为空').notEmpty();
 	req.checkBody('email', '电子邮件不能为空').notEmpty();
 	req.checkBody('email', '不是有效的电子邮件地址').isEmail();
 	req.checkBody('username', '用户名不能为空').notEmpty();
+	req.checkBody('username', '用户名不能为空').len(8, 32);
 	req.checkBody('password', '密码不能为空').notEmpty();
 	req.checkBody('password2', '密码两次输入不一致').equals(req.body.password);
 	req.checkBody('phone', '手机号不能为空').notEmpty();
@@ -52,17 +54,17 @@ router.post('/register', function (req, res) {
 	req.getValidationResult().then(function (result) {
 		if (!result.isEmpty()) {
 			res.render('users/register', {
-				errors: result.array()
+				errors: result.array(),
+				user_info
 			});
 		} else {
 			console.log('PASSED');
 			var newUser = new User({
-				username: username,
-				name: name,
-				email: email,
-				password: password,
-				phone: phone,
-				company: company,
+				username: user_info.username,
+				name: user_info.name,
+				email: user_info.email,
+				phone: user_info.phone,
+				company: user_info.company,
 				group: group,
 				appkey: '',
 				appsecret: '',
@@ -166,13 +168,17 @@ router.post('/app/api/link', User.ensureAuthenticated, function (req, res) {
 passport.use(new LocalStrategy(
 	function (username, password, done) {
 		User.getUserByUsername(username, function (err, user) {
-			if (err) throw err;
+			if (err) {
+				return done(null, false, { message: err });
+			}
 			if (!user) {
 				return done(null, false, { message: '用户不存在' });
 			}
 
 			User.comparePassword(password, user.password, function (err, isMatch) {
-				if (err) throw err;
+				if (err) {
+					return done(null, false, { message: err })
+				}
 				if (isMatch) {
 					if (User.isUserActivated(user.activation)) {
 						return done(null, user);
