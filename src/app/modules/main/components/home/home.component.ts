d01aaf82f140aaecd6f2d773e802286280ade380
Yuancheng Zhang <old.far.hot@gmail.com>
2017-08-28 03:06:28 -0500

    添加了Home Component

---
+++ src/app/modules/main/components/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component, AfterViewInit } from '@angular/core';
+/// <reference path="jquery.d.ts" />
+
+@Component({
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements AfterViewInit {
+    constructor() {}
+
+    ngAfterViewInit() {
+    }
+}
