d09d78718c1a36404e038be5b0ea89151d3eb34b
Yuancheng Zhang <old.far.hot@gmail.com>
2017-08-28 20:52:12 -0500

    remove something useless

--- src/app/modules/main/components/about/about.component.ts
+++ src/app/modules/main/components/about/about.component.ts
@@ -1,50 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations';
-import { TimerObservable } from "rxjs/observable/TimerObservable";
-import { Subscription } from "rxjs/Subscription";
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-about',
     templateUrl: './about.component.html',
-    styleUrls: ['./about.component.scss'],
-    animations: [
-        trigger('flyInOut', [
-            state('in', style({
-                opacity: 1,
-                transform: 'translateX(0)'
-            })),
-            state('out', style({
-                opacity: 0,
-                transform: 'translateX(50%)'
-            })),
-            transition('in => out', animate('200ms ease-in', style(
-                {
-                    transform: 'translateX(-50%)'
-                }
-            ))),
-            transition('out => in', animate('200ms ease-out'))
-        ])
-    ]
+    styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements OnInit {
 
-    hideAppointment: boolean = true;
-    nowPageNumber: number = 1;
-
-    private subscription3s: Subscription;
-    constructor() { }
-
-    ngOnInit() {
-        let timer3s = TimerObservable.create(5000, 5000);
-        this.subscription3s = timer3s.subscribe(this.refresh3s.bind(this));
-    }
-
-    changePage(i: number) {
-        this.nowPageNumber = i;
-    }
-
-    refresh3s(t: number) {
-        this.nowPageNumber = this.nowPageNumber % 2 + 1;
-    }
-
-}
+export class AboutComponent { }
