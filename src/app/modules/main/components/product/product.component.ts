daef1fbe959ea98beab152d59d09061965464f1a
Yuancheng Zhang <old.far.hot@gmail.com>
2017-09-06 16:03:52 -0500

    add a hidden product single page

--- src/app/modules/main/components/product/product.component.ts
+++ src/app/modules/main/components/product/product.component.ts
@@ -5,36 +5,10 @@ import { Component, OnInit } from '@angular/core';
     templateUrl: './product.component.html',
     styleUrls: ['./product.component.scss']
 })
-export class ProductComponent implements OnInit {
-    hideEducation: boolean = true;
-    hideGame: boolean = true;
-    hideAnimate: boolean = true;
-    hideHardware: boolean = true;
-
+export class ProductComponent {
     nowIndex = 0;
+    isSinglePage = false;
 
     constructor() { }
 
-    ngOnInit() {
-    }
-
-    changeFeature(index: number) {
-        this.nowIndex = index;
-    }
-
-    showOrCloseEducation() {
-        this.hideEducation = !this.hideEducation;
-    }
-
-    showOrCloseGame() {
-        this.hideGame = !this.hideGame;
-    }
-
-    showOrCloseAnimate() {
-        this.hideAnimate = !this.hideAnimate;
-    }
-
-    showOrCloseHardware() {
-        this.hideHardware = !this.hideHardware;
-    }
 }
