e0147b9d25afb53489aff0f9eddf6ad8d0b7b022
Yuancheng Zhang <old.far.hot@gmail.com>
2017-09-06 19:10:07 -0500

    实现了“产品方案”页面“点击按钮出现单页、滑出界面隐藏单页”的重要功能

--- src/app/modules/main/components/product/product.component.ts
+++ src/app/modules/main/components/product/product.component.ts
@@ -1,14 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
     selector: 'app-product',
     templateUrl: './product.component.html',
     styleUrls: ['./product.component.scss']
 })
-export class ProductComponent {
+export class ProductComponent implements AfterViewInit {
     nowIndex = 0;
-    isSinglePage = false;
-
     constructor() { }
 
+    ngAfterViewInit() {
+
+        window.onscroll = function (e) {
+            console.log("Rolling.....");
+
+            if ($('#product-s').is(':visible')) {
+                var pageTop = $(window).scrollTop();
+                var pageBottom = pageTop + $(window).height();
+                var prodSingleTop = $('#product-s').offset().top;
+                var prodSingleBottom = prodSingleTop + $('#product-s').height();
+                console.log(`${pageTop}, ${pageBottom}; ${prodSingleTop}, ${prodSingleBottom};`);
+                if ((pageTop > prodSingleBottom) || (pageBottom < prodSingleTop)) {
+                    console.log('out view');
+                    $('#product-s').hide();
+                    $('#product').show();
+                } else {
+                    console.log('in view');
+                }
+            }
+        }
+    }
+
+    clickProdItem(i: number): void {
+        $('#product-s').show();
+        $('#product').hide();
+        if ($('#product-s').is(':visible')) {
+            console.log('is visable');
+            window.scrollTo(0, $('#product-s').offset().top);
+        } else {
+            console.log('is not visialbe');
+        }
+    }
 }
