3db16e6e0e583cff29125ee59efe1bf8864c8df8
Yuancheng Zhang <old.far.hot@gmail.com>
2017-09-06 19:11:24 -0500

    移除了上一个commit中debug用的code和comment

--- src/app/modules/main/components/product/product.component.ts
+++ src/app/modules/main/components/product/product.component.ts
@@ -12,20 +12,15 @@ export class ProductComponent implements AfterViewInit {
     ngAfterViewInit() {
 
         window.onscroll = function (e) {
-            console.log("Rolling.....");
-
             if ($('#product-s').is(':visible')) {
                 var pageTop = $(window).scrollTop();
                 var pageBottom = pageTop + $(window).height();
                 var prodSingleTop = $('#product-s').offset().top;
                 var prodSingleBottom = prodSingleTop + $('#product-s').height();
-                console.log(`${pageTop}, ${pageBottom}; ${prodSingleTop}, ${prodSingleBottom};`);
+                // console.log(`${pageTop}, ${pageBottom}; ${prodSingleTop}, ${prodSingleBottom};`);
                 if ((pageTop > prodSingleBottom) || (pageBottom < prodSingleTop)) {
-                    console.log('out view');
                     $('#product-s').hide();
                     $('#product').show();
-                } else {
-                    console.log('in view');
                 }
             }
         }
@@ -35,10 +30,7 @@ export class ProductComponent implements AfterViewInit {
         $('#product-s').show();
         $('#product').hide();
         if ($('#product-s').is(':visible')) {
-            console.log('is visable');
             window.scrollTo(0, $('#product-s').offset().top);
-        } else {
-            console.log('is not visialbe');
         }
     }
 }
