2f15e1dfd04c92835c098dce48019e6b10917f94
Yuancheng Zhang <old.far.hot@gmail.com>
2017-09-10 23:25:44 -0500

    contact: 修复了邮箱发邮件的bug（可能还有问题）

--- src/app/modules/main/components/contact/contact.component.ts
+++ src/app/modules/main/components/contact/contact.component.ts
@@ -1,31 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-contact',
     templateUrl: './contact.component.html',
     styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent implements OnInit {
-
-    name:string = "";
-    mailto:string = "";
-    content:string = "";
-
-    constructor() {
-
-    }
-    ngOnInit() {}
-
-    onEnterName(value:string) {
-        this.name = value;
-        console.log(this.name);
-    }
-
-    onEnterMailto(value:string) {
-        this.mailto = value;
-    }
-
-    onEnterContent(value:string){
-        this.content = value;
-    }
-}
+export class ContactComponent {}
