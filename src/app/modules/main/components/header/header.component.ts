6ef9adb53260606f98bde8ea2a6f98037015a386
Yuancheng Zhang <old.far.hot@gmail.com>
2017-09-15 01:20:55 -0500

    Header：完成中英模版

--- src/app/modules/main/components/header/header.component.ts
+++ src/app/modules/main/components/header/header.component.ts
@@ -1,18 +1,75 @@
-import { Component, AfterViewInit } from '@angular/core';
-/// <reference path="jquery.d.ts" />
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+
+interface Header {
+    home: string;
+    tech: string;
+    product: string;
+    partner: string;
+    about: string;
+    career: string;
+    contact: string;
+}
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements AfterViewInit {
-    constructor() {}
+export class HeaderComponent implements OnInit {
+    language = 'UNKNOWN';
+    langIndex = 0;
 
-    ngAfterViewInit() {}
+    nowHeader: Header;
+    headers: [Header] = [
+        {
+            home: '首页',
+            tech: '核心技术',
+            product: '产品方案',
+            partner: '合作客户',
+            about: '关于齐悟',
+            career: '加入齐悟',
+            contact: '联系我们'
+        },
+        {
+            home: 'Home',
+            tech: 'Tech',
+            product: 'Product',
+            partner: 'Partner',
+            about: 'About',
+            career: 'Career',
+            contact: 'Contact'
+        }
+    ];
+
+    constructor(private cookieService: CookieService) {
+        this.language = this.cookieService.get('lang');
+        if (this.language === 'en') {
+            this.langIndex = 1;
+        } else {
+            this.langIndex = 0;
+        }
+        this.nowHeader = this.headers[this.langIndex];
+    }
+
+    ngOnInit() {
+        if (!this.cookieService.get('lang')) {
+            this.cookieService.set('lang', 'zh');
+        }
+        this.language = this.cookieService.get('lang');
+    }
 
     changeActive(index: number) {
         $('#header-nav-bar ul li').removeClass('active');
         $(`#header-nav-bar ul li:nth-child(${index})`).addClass('active');
+    }
+
+    changeLanguage(language: string) {
+        if (language === 'en') {
+            this.cookieService.set('lang', 'en');
+        } else {
+            this.cookieService.set('lang', 'zh');
+        }
+        location.reload();
     }
 }
