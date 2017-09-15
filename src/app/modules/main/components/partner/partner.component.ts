88a0289117aabe3f3f040df0cc6785261cda06b0
Yuancheng Zhang <old.far.hot@gmail.com>
2017-09-15 05:04:58 -0500

    合作伙伴：更新背景动画

--- src/app/modules/main/components/partner/partner.component.ts
+++ src/app/modules/main/components/partner/partner.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
     selector: 'app-partner',
     templateUrl: './partner.component.html',
     styleUrls: ['./partner.component.scss']
 })
-export class PartnerComponent {
-    constructor() {}
+export class PartnerComponent implements AfterViewInit {
+    ngAfterViewInit() {
+        window.onscroll = function (e) {
+            const pageTop = $(window).scrollTop();
+            const pageBottom = pageTop + $(window).height();
+            const pageWidth = $(window).width();
+            const partnerTop = $('#partner').offset().top;
+            const partnerBottom = partnerTop + $('#partner').height();
+            const partnerWidth = $('#partner').width();
+            console.log(`${pageTop}, ${pageBottom}; ${partnerTop}, ${partnerBottom}; ${pageWidth}, ${partnerWidth};`);
+            if (!((pageTop > partnerBottom) || (pageBottom < partnerTop))) {
+                $('#partner-bg-left').css('top', (pageTop - partnerTop) / 8 + 80 + 'px');
+                $('#partner-bg-right').css('top', (pageTop - partnerTop) / -4 + 80 + 'px');
+                let bg_img_x = 50;
+                let bg_img_y = 50;
+                bg_img_x -= (pageTop - partnerTop) / 25;
+                bg_img_y += (pageTop - partnerTop) / 15;
+                if (bg_img_x < 0) { bg_img_x = 0; }
+                if (bg_img_x > 100) { bg_img_x = 100; }
+                if (bg_img_y < 0) { bg_img_y = 0; }
+                if (bg_img_y > 100) { bg_img_y = 100; }
+                $('#partner').css('background-position', bg_img_x + '% ' + bg_img_y + '%');
+                console.log($('#partner').css('backgroundPosition'));
+            }
+        }
+    }
 }
