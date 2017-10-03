44a27288c7aee4948c4da0fb665bec6b6805ba86
Yuancheng Zhang <old.far.hot@gmail.com>
2017-10-03 09:54:33 -0500

    将粒子动画更新到test页面里

--- src/app/modules/test/components/about/about.component.ts
+++ src/app/modules/test/components/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit, OnChanges, Input, ViewChild, ElementRef, ChangeDetectorRef, NgZone } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 
 interface About {
@@ -7,12 +7,17 @@ interface About {
     desc: string;
 }
 
+enum Side {
+    Left = 1,
+    Right
+}
+
 @Component({
     selector: 'app-about',
     templateUrl: './about.component.html',
     styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements AfterViewInit {
+export class AboutComponent implements OnInit, AfterViewInit, OnDestroy {
     language = 'UNKNOWN';
     langIndex = 0;
 
@@ -41,7 +46,19 @@ export class AboutComponent implements AfterViewInit {
     ];
     nowPic: string;
 
-    constructor(private cookieService: CookieService) {
+    private _sWidth: number;
+    private _sHeight: number;
+    private ctx: CanvasRenderingContext2D;
+    private running: boolean;
+    private dotNumber: number;
+    private isViewInit: boolean;
+
+    public dots: Array<Dot>;
+
+    // get the element with the #canvasRef on it
+    @ViewChild('testCanvas') canvasRef: ElementRef;
+
+    constructor(private cookieService: CookieService, private ngZone: NgZone) {
         this.language = this.cookieService.get('lang');
         if (this.language === 'en') {
             this.langIndex = 1;
@@ -50,11 +67,42 @@ export class AboutComponent implements AfterViewInit {
         }
         this.nowAbout = this.abouts[this.langIndex];
         this.nowPic = this.pictures[this.picIndex];
+
+        this.isViewInit = false;
+        const getWiddowDims = () => {
+            if ($(window).width() > 1000) {
+                this._sWidth = $(window).width();
+            } else {
+                this._sWidth = 1000;
+            }
+            this._sHeight = 694;
+            if (this.isViewInit) { };
+        };
+
+        window.onresize = () => {
+            getWiddowDims();
+        };
+
+        getWiddowDims();
+    }
+
+    ngOnInit() {
+        this.running = true;
+        this.ctx = this.canvasRef.nativeElement.getContext('2d');
+        this.dotNumber = 10;
+    }
+
+    ngOnDestroy(): void {
+        this.running = false;
     }
 
     ngAfterViewInit() {
         this.changePicLeft();
+        this.isViewInit = true;
+        this.dots = this.initDots(this.dotNumber, this._sWidth, this._sHeight);
+        this.animate();
     }
+
 
     changePicLeft() {
         setTimeout(() => {
@@ -62,5 +110,141 @@ export class AboutComponent implements AfterViewInit {
             this.nowPic = this.pictures[this.picIndex];
             this.changePicLeft();
         }, 2000);
+    }
+
+
+    private initDots(cnt: number, sWidth: number, sHeight: number): Array<Dot> {
+        const dots: Array<Dot> = new Array<Dot>();
+        for (let i = 0; i < cnt; ++i) {
+            if (i < cnt / 2) {
+                dots.push(new Dot(Side.Left, this._sWidth, this._sHeight));
+            } else {
+                dots.push(new Dot(Side.Right, this._sWidth, this._sHeight));
+            }
+        }
+        return dots;
+    }
+
+    private clear() {
+        this.ctx.clearRect(0, 0, this._sWidth, this._sHeight);
+    }
+
+    private animate() {
+        // this.clear();
+        this.ctx.fillStyle = 'rgba(255,255,255,.1)';
+        this.ctx.fillRect(0, 0, this._sWidth, this._sHeight);
+        for (let i = 0; i < this.dots.length; ++i) {
+            const d = this.dots[i];
+            d.update();
+            if (d.state > 3 || d.y > this._sHeight || d.y < 0) {
+                if (i < this.dotNumber / 2) {
+                    this.dots[i] = new Dot(Side.Left, this._sWidth, this._sHeight);
+                } else {
+                    this.dots[i] = new Dot(Side.Right, this._sWidth, this._sHeight);
+                }
+            } else {
+                d.draw(this.ctx, this._sWidth, this._sHeight);
+            }
+        }
+
+        if (this.running) {
+            // Schedule next
+            requestAnimationFrame(() => {
+                this.animate();
+            });
+        }
+    }
+}
+
+class Dot {
+    x: number;
+    y: number;
+    dx: number;
+    dy: number;
+    radius: number;
+    speed: number;
+    scale: number;
+    tscale: number;
+    tracks: [number];
+    state: number;
+    alpha: number;
+
+    constructor(side: Side, sWidth: number, sHeight: number) {
+        if (side === Side.Left) {
+            this.x = 0;
+            this.dx = 1;
+        } else {
+            this.x = sWidth;
+            this.dx = -1;
+        }
+        // 在全屏幕的高度的中间80%区域生成点
+        this.y = Math.random() * sHeight * 0.8 + sHeight * 0.1;
+        if (this.y < sHeight / 2) {
+            this.dy = 1; // turn down
+        } else {
+            this.dy = -1; // turn up
+        }
+        this.radius = 1;
+        this.speed = Math.random() * 0.6 + 0.8;
+        this.state = 0;
+
+        this.scale = Math.random() * 2 + 1;
+        this.tscale = this.scale * 3;
+
+        const track0 = Math.random() * sWidth * .2 + sWidth * .05;
+        const track1 = Math.random() * sWidth * .1 + sWidth * .05 + track0;
+        const track2 = Math.random() * sWidth * .3 + sWidth * .05 + track1;
+        if (side === Side.Left) {
+            this.tracks = [track0, track1, track2];
+        } else {
+            this.tracks = [sWidth - track0, sWidth - track1, sWidth - track2];
+        }
+        this.alpha = Math.random() * 0.3 + 0.7;
+    }
+
+    public update() {
+        switch (this.state) {
+            case 0: {
+                this.x += this.speed * this.dx;
+                if (Math.abs(this.x - this.tracks[0]) <= 1) {
+                    this.state++;
+                }
+                break;
+            }
+            case 1: {
+                this.x += this.speed * this.dx;
+                this.y += this.speed * this.dy;
+                if (Math.abs(this.x - this.tracks[1]) <= 1) {
+                    this.state++;
+                }
+                break;
+            }
+            case 2: {
+                this.x += this.speed * this.dx;
+                if (Math.abs(this.x - this.tracks[2]) <= 1) {
+                    this.state++;
+                }
+                break;
+            }
+            case 3: {
+                this.scale *= 1.02;
+                if (this.scale > this.tscale) {
+                    this.state++;
+                }
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+    }
+
+    public draw(ctx: CanvasRenderingContext2D, sWidth: number, sHeight: number): void {
+        ctx.save();
+        ctx.beginPath();
+        ctx.arc(sWidth / 2 + (this.x - sWidth / 2), sHeight / 2 + (this.y - sHeight / 2), this.radius * this.scale, 0, Math.PI * 2);
+        ctx.fillStyle = `rgba(10, 84, 111, ${this.alpha})`;
+        ctx.fill();
+        ctx.restore();
     }
 }
