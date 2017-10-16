43b5dc430b29d605149586f9c4b39f1dc5ddf949
Yuancheng Zhang <old.far.hot@gmail.com>
2017-10-16 07:17:08 -0500

    更新粗粗文字信息

--- src/app/modules/desktop/components/product/product.component.ts
+++ src/app/modules/desktop/components/product/product.component.ts
@@ -202,7 +202,7 @@ export class ProductComponent implements AfterViewChecked {
                             img: ['/assets/img/product_cases/vi_cc_01.png'],
                             link: [{
                                 ref: 'https://www.centaurstech.com/product/cc/',
-                                name: 'CC'
+                                name: '醋醋官网'
                             }],
                             video: []
                         }
@@ -382,7 +382,7 @@ export class ProductComponent implements AfterViewChecked {
                             img: ['/assets/img/product_cases/vi_cc_01.png'],
                             link: [{
                                 ref: 'https://www.centaurstech.com/product/cc/',
-                                name: 'CC'
+                                name: 'CuCu'
                             }],
                             video: []
                         }
