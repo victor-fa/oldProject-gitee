4cd99c46e866397c5ee3a908e52758dc7ac857c9
Yuancheng Zhang <old.far.hot@gmail.com>
2017-10-25 11:55:46 -0500

    completes mobile header menu

--- src/app/modules/mobile/components/header-m/header-m.component.ts
+++ src/app/modules/mobile/components/header-m/header-m.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-header-m',
-  templateUrl: './header-m.component.html',
-  styleUrls: ['./header-m.component.scss']
+    selector: 'app-header-m',
+    templateUrl: './header-m.component.html',
+    styleUrls: ['./header-m.component.scss']
 })
 export class HeaderMComponent {
-  constructor() {
-  }
+    openSideNav() {
+        $('#header-m-nav-left').show();
+    }
+
+    closeSideNav() {
+        $('#header-m-nav-left').hide();
+        // console.log(12341234);
+    }
 }
