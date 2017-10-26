39a81d76cc3b297297a0ef01241976a0d8ea3a99
Yuancheng Zhang <old.far.hot@gmail.com>
2017-10-26 14:54:24 -0500

    partner-m: add component

--- src/app/modules/mobile/components/partner-m/partner-m.component.ts
+++ src/app/modules/mobile/components/partner-m/partner-m.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-partner-m',
-  templateUrl: './partner-m.component.html',
-  styleUrls: ['./partner-m.component.scss']
+    selector: 'app-partner-m',
+    templateUrl: './partner-m.component.html',
+    styleUrls: ['./partner-m.component.scss']
 })
 export class PartnerMComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
