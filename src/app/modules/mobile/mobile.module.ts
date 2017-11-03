0891f15813d28dd89189a25e2cee514015952a48
Yuancheng Zhang <old.far.hot@gmail.com>
2017-11-03 16:31:29 -0500

    team-m: removed

--- src/app/modules/mobile/mobile.module.ts
+++ src/app/modules/mobile/mobile.module.ts
@@ -8,7 +8,6 @@ import { HeaderMComponent } from './components/header-m/header-m.component';
 import { AboutMComponent } from './components/about-m/about-m.component';
 import { PlanMComponent } from './components/plan-m/plan-m.component';
 import { PartnerMComponent } from './components/partner-m/partner-m.component';
-import { TeamMComponent } from './components/team-m/team-m.component';
 import { ContactMComponent } from './components/contact-m/contact-m.component';
 import { CareerMComponent } from './components/career-m/career-m.component';
 import { HomeMComponent } from './components/home-m/home-m.component';
