d96f9044e2f3516dc62635911fa582e3646830e9
Yuancheng Zhang <old.far.hot@gmail.com>
2017-12-06 11:46:48 -0600

    revise p5.js => typescript

--- src/app/modules/test/components/home/home.component.ts
+++ src/app/modules/test/components/home/home.component.ts
@@ -4,8 +4,6 @@ import { CookieService } from 'ngx-cookie-service';
 import { HomeTitleZhComponent } from "./title/title-zh.component";
 import { HomeTitleEnComponent } from "./title/title-en.component";
 
-import { Star } from './star';
-
 import * as p5 from 'p5';
 
 export interface HomeTitle {
@@ -31,92 +29,84 @@ export class HomeComponent implements OnInit {
 
     ngOnInit() {
         const p5_opt = (p) => {
-
-            let canvas;
-            let constellation = [];
-            let n;
-            let d;
+            let width: number = 1000;
+            let height: number = 619;
+            let canvas: p5.Renderer2D;
+            let constellation: Array<Star>;
+            let n: number;
+            let d: number;
 
             p.setup = () => {
-                canvas = p.createCanvas(1000, 619);
+                canvas = p.createCanvas(500, 500);
                 p.pixelDensity(1); // Set 1 because it's too slow on firefox
                 //pixelDensity(displayDensity());
                 canvas.parent('equalizer');
-                let n = 20;
-
+                n = 100;
+                constellation = [];
                 for (var i = 0; i < n; i++) {
                     constellation.push(new Star());
                 }
                 p.strokeWeight(.75);
                 p.stroke('#0000FF');
-                console.log(constellation);
             }
 
             p.draw = () => {
-                p.background(255, 0, 255, 1);
-                for (let k = 0; k < constellation.length; k++) {
-                    constellation[k].update();
-                    // p.ellipse(constellation[k].x, constellation[k].y, 80, 80);
-                }
-                // p.ellipse(p.mouseX, p.mouseY, 80, 80);
-                /*
-                // background('#000000');
-                p.background('#FF00FF');
+                // p.background(255, 0, 255, 1);
+                p.background('#FFFFFF');
 
-                for (let i = 0; i < p.constellation.length; i++) {
-                    p.constellation[i].update();
-                    for (var j = 0; j < p.constellation.length; j++) {
+                for (var i = 0; i < constellation.length; i++) {
+                    constellation[i].update();
+                    for (var j = 0; j < constellation.length; j++) {
                         if (i > j) { // "if (i > j)" => to check one time distance between two stars
-                            d = p.constellation[i].loc.dist(constellation[j].loc); // Distance between two stars
-                            if (d <= p.width / 10) { // if d is less than width/10 px, we draw a line between the two stars
+                            d = constellation[i].loc.dist(constellation[j].loc); // Distance between two stars
+                            if (d <= width / 10) { // if d is less than width/10 px, we draw a line between the two stars
                                 p.line(constellation[i].loc.x, constellation[i].loc.y, constellation[j].loc.x, constellation[j].loc.y)
                             }
                         }
                     }
                 }
-                */
             }
 
             class Star {
-                width: number;
-                height: number;
-                x: number;
-                y: number;
                 a: number;
                 r: number;
                 loc: p5.Vector;
                 speed: p5.Vector;
                 bam: p5.Vector;
-                m;
-
-                /*
-                this.a = random(5 * TAU); // "5*TAU" => render will be more homogeneous
-                this.r = random(width * .2, width * .25); // first position will looks like a donut
-                this.loc = createVector(width / 2 + sin(this.a) * this.r, height / 2 + cos(this.a) * this.r);
-                this.speed = createVector();
-                this.speed = p5.Vector.random2D();
-                //this.speed.random2D();
-                this.bam = createVector();
-                this.m;
-                */
+                m: number;
 
                 constructor() {
-                    this.width = 1000;
-                    this.height = 619;
                     this.a = p.random(5 * p.TAU); // "5*TAU" => render will be more homogeneous
-                    this.r = p.random(this.width * .2, this.width * .25); // first position will looks like a donut
-                    this.loc = p.createVector(this.width / 2 + p.sin(this.a) * this.r, p.height / 2 + p.cos(this.a) * this.r);
-                    // this.speed = p.createVector();
+                    this.r = p.random(p.width * .2, p.width * .25); // first position will looks like a donut
+                    this.loc = p.createVector(p.width / 2 + p.sin(this.a) * this.r, p.height / 2 + p.cos(this.a) * this.r);
                     this.speed = p5.Vector.random2D();
-                    //this.speed.random2D();
                     this.bam = p.createVector();
                     this.m;
                 }
                 update() {
-                    this.x += 20;
-                    this.y += 15;
-                    this.x %= 1000;
-                    this.y %= 619;
+                    this.bam = p5.Vector.random2D(); // movement of star will be a bit erractic
+                    //this.bam.random2D();
+                    this.bam.mult(0.45);
+                    this.speed.add(this.bam);
+                    // speed is done according distance between loc and the mouse :
+                    this.m = p.constrain(p.map(p.dist(this.loc.x, this.loc.y, p.mouseX, p.mouseY), 0, p.width, 8, .05), .05, 8); // constrain => avoid returning "not a number"
+                    this.speed.normalize().mult(this.m);
+
+                    // No colision detection, instead loc is out of bound
+                    // it reappears on the opposite side :
+                    if (p.dist(this.loc.x, this.loc.y, p.width / 2, p.height / 2) > (p.width / 2) * 0.98) {
+                        if (this.loc.x < p.width / 2) {
+                            this.loc.x = p.width - this.loc.x - 4; // "-4" => avoid blinking stuff
+                        } else if (this.loc.x > p.width / 2) {
+                            this.loc.x = p.width - this.loc.x + 4; // "+4"  => avoid blinking stuff
+                        }
+                        if (this.loc.y < p.height / 2) {
+                            this.loc.y = p.width - this.loc.y - 4;
+                        } else if (this.loc.x > p.height / 2) {
+                            this.loc.y = p.width - this.loc.y + 4;
+                        }
+                    }
+                    this.loc = this.loc.add(this.speed);
                 }
             }
         }
