18484b9f6a031afbdb40b68732212abce68729af
shenxiaofa <xf827542599@163.com>
2018-11-06 21:09:33 +0800

    下载接口添加跨域功能

--- routes/download.js
+++ routes/download.js
@@ -4,6 +4,11 @@ var dbData = require('../services/download')
 
 /* GET getCount. */
 router.get('/getCount', (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1');
+    res.header("Content-Type", "application/json;charset=utf-8");
     dbData.selectall('Calculation', function (result) {
         res.send(result);
     })
@@ -11,6 +16,11 @@ router.get('/getCount', (req, res, next) => {
 
 /* GET addCountForWin7. */
 router.get('/addCountForWin7', (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1');
+    res.header("Content-Type", "application/json;charset=utf-8");
     dbData.selectall('Calculation', function (result) {
         if (result !== null) {
             const countForWin7 = result[0].countForWin7;
@@ -23,6 +33,11 @@ router.get('/addCountForWin7', (req, res, next) => {
 
 /* GET addCountForWin10. */
 router.get('/addCountForWin10', (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1');
+    res.header("Content-Type", "application/json;charset=utf-8");
     dbData.selectall('Calculation', function (result) {
 
         if (result !== null) {
